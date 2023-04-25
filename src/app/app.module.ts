0f141c98fd8879b1b2c070153c73b631bbe403eb
Isbeli Ramirez <isbeliramirez8@gmail.com>
2023-04-25 18:18:02 -0400

    Chore: project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header.component';
+import { TweetComponent } from './views/tweet/tweet.component';
+import { PostComponent } from './views/post/post.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './views/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TweetComponent,
+    PostComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/post/post.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+  tweetMessage:   string = ""
+  tweetMaxLength: number = 240
+  tweetRemain:    number = this.tweetMaxLength
+
+  sendTweet(){
+    alert(this.tweetMessage)
+  }
+
+  changeTweet(){ 
+    this.tweetRemain = 240 - this.tweetMessage.length
+  }
+}
